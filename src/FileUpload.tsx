a270b21c4bb2c6189eac17aecd5ed7c3196522c8
sarant1 <samuel.arant10@gmail.com>
2023-02-09 20:40:38 -0500

    figured out how to upload via formdata to s3

--- src/FileUpload.tsx
+++ src/FileUpload.tsx
@@ -1,15 +1,34 @@
 import { SyntheticEvent, useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import axios from "axios";
 
 export function FileUpload() {
   const [file, setFile] = useState(null);
+  let fd: any = new FormData();
 
   function handleUpload() {
-    console.log(file);
+    const response: any = axios
+      .get(
+        "https://hf1crtnxu8.execute-api.us-east-1.amazonaws.com/v1/getsignedurl"
+      )
+      .then((res) => {
+        for (var key in res["data"]["fields"]) {
+          fd.append(key, res["data"]["fields"][key]);
+        }
+
+        fd.append("file", file);
+        axios.post(res["data"]["url"], fd, {
+          headers: {
+            "content-type": "multipart/xml",
+          },
+        });
+      })
+      .catch((err) => console.log(err));
   }
   function handleChange(e: any) {
     setFile(e.target.files[0]);
-    console.log(e.target.files);
+
+    fd.append("pdf-file", file);
   }
 
   return (
