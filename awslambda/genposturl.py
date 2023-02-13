28b8b876c45cdbe787054f079a156593ecb30866
sarant1 <samuel.arant10@gmail.com>
2023-02-12 23:04:55 -0500

    added id to lambda function

--- awslambda/genposturl.py
+++ awslambda/genposturl.py
@@ -1,18 +1,24 @@
 import boto3
 from botocore.exceptions import ClientError
 import json
+import uuid
 
 client = boto3.client('s3')
+
+pdfid = str(uuid.uuid4())
+
 
 def lambda_handler(event, context):
     try:
         response = client.generate_presigned_post(
             'docfilestobeconverted',
             'moneyovermoney',
+            Fields= {
+                'pdfid': pdfid,
+            },
             ExpiresIn=3600
             )
     except ClientError as e:
         logging.error(e)
         return None
     return response
-
